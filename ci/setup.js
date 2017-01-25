33abe63f719980bec9c12a75b53e4b3189d630f1
FujiHaruka <fujiharuka@realglobe.jp>
2017-01-25 22:40:13 +0900

    setup.js

--- ci/setup.js
+++ ci/setup.js
@@ -1,10 +1,30 @@
 #!/usr/bin/env node
 const { join } = require('path')
 const { execSync } = require('child_process')
+const fs = require('fs')
 const mkdirp = require('mkdirp')
 
 process.chdir(join(__dirname, '..'))
 
 mkdirp.sync('repos')
 
-// TODO git clone ...
+let repos = [
+  'ever-increasing-faith',
+  'faith-that-prevails',
+  'andrew-murray-humility',
+  'first-clement'
+]
+
+let cloned = fs.readdirSync('repos')
+
+repos.forEach(repo => {
+  if (cloned.includes(repo)) {
+    return
+  }
+  let command = `git clone https://github.com/christian-classics-jp/${repo}.git`
+  console.log('>', command)
+  let out = execSync(command, {
+    cwd: join(__dirname, '../repos')
+  }).toString()
+  console.log(out)
+})
